2d1fef35ad8b91c56c29e1c683e93a88cd3e1c7e
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2018-12-23 21:59:46 -0800

    Updates

---
+++ precache-manifest.477c7ba727c52ae26f363b1d94baa0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81ff266797e27bb5f6f9",
+    "url": "/starbattle/static/css/main.c420491c.chunk.css"
+  },
+  {
+    "revision": "81ff266797e27bb5f6f9",
+    "url": "/starbattle/static/js/main.81ff2667.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/starbattle/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "38031c142c8c557fd919",
+    "url": "/starbattle/static/js/runtime~main.38031c14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starbattle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "46c0bad3735d8c48e273227c63c504bc",
+    "url": "/starbattle/index.html"
+  }
+];
