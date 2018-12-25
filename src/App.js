1a0315d16abcca84a24d0b0aea2cca67c3702c55
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2018-12-24 22:26:08 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,105 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import RefreshIcon from '@material-ui/icons/Refresh'
+import Modal from '@material-ui/core/Modal'
+import InfoIcon from '@material-ui/icons/Info'
+import getGrid from './gridUtils'
+import './App.css'
+
+const colors = [
+  '#f9e79f',
+  '#a9dfbf',
+  '#a3e4d7',
+  '#a9cce3',
+  '#d7bde2',
+  '#e6b0aa',
+  '#edbb99',
+  '#fad7a0',
+  '#a2d9ce',
+  '#aed6f1',
+  '#d2b4de',
+  '#f5b7b1',
+]
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    const LEVEL = 9
+    this.state = {
+      level: LEVEL,
+      modalOpen: false,
+      grid: getGrid(LEVEL),
+    }
+  }
+
+  toggleModal = () => this.setState({ modalOpen: !this.state.modalOpen })
+
   render() {
+    const { grid } = this.state
+    console.log('grid', grid)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <AppBar className='appBar' position='static'>
+          <Toolbar>
+            <Typography variant='h6' color='inherit'>
+              ★ Star Battle
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div
+          style={{
+            display: 'block',
+            display: 'flex',
+            flexWrap: 'wrap',
+            padding: '15px',
+            justifyContent: 'center',
+          }}
+        >
+          {grid.map((row, i) =>
+            row.map((col, j) => (
+              <div
+                style={{
+                  background: colors[grid[i][j]],
+                  width: `${85 / this.state.level}vw`,
+                  height: `${85 / this.state.level}vw`,
+                  margin: '1px',
+                }}
+              />
+            )),
+          )}
+        </div>
+        <BottomNavigation className='navbar' showLabels>
+          <BottomNavigationAction
+            label='About'
+            icon={<InfoIcon />}
+            onClick={this.toggleModal}
+          />
+          <BottomNavigationAction
+            label='Points'
+            icon={<InfoIcon />}
+            onClick={() => {}}
+          />{' '}
+          <BottomNavigationAction
+            label='Reset'
+            icon={<RefreshIcon />}
+            onClick={() => {}}
+          />
+        </BottomNavigation>
+        <Modal
+          aria-labelledby='simple-modal-title'
+          aria-describedby='simple-modal-description'
+          open={this.state.modalOpen}
+          onClose={this.toggleModal}
+        >
+          <div className='modal'>hi this is modal </div>
+        </Modal>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/gridUtils.js
@@ -0,0 +1,115 @@
+const getGrid = gridSize => {
+  const initStars = gridSize => {
+    let grid = [Math.floor(Math.random() * gridSize)];
+    while (grid.length < gridSize) {
+      const safeNums = [
+        ...new Set(
+          new Array(gridSize)
+            .fill(1)
+            .map((_, i) => i)
+            .filter(item => grid.indexOf(item) === -1)
+            .filter(item => item !== grid[grid.length - 1] + 1)
+            .filter(item => item !== grid[grid.length - 1] - 1)
+            .map(JSON.stringify)
+        )
+      ].map(JSON.parse);
+      if (safeNums.length > 0) {
+        grid.push(safeNums[Math.floor(Math.random() * safeNums.length)]);
+      } else {
+        grid = [Math.floor(Math.random() * gridSize)];
+      }
+    }
+    return grid;
+  };
+
+  function printStars(stars = []) {
+    stars.forEach(item => {
+      let str = Array(stars.length).fill(".");
+      str[item] = `${item}`;
+      console.log(str);
+    });
+  }
+  const grid = initStars(gridSize);
+  // console.log(grid)
+  // printStars(grid)
+  const createAreas = stars => {
+    const initGrid = stars.map(star => {
+      const row = Array(stars.length).fill(undefined);
+      row[star] = star;
+      return row;
+    });
+
+    while (!isFilled(initGrid)) {
+      //radomlly pick a region id
+      const regionId = Math.floor(Math.random() * stars.length);
+      //find current regions
+      const regions = [];
+      initGrid.forEach((row, i) =>
+        row.forEach((col, j) => {
+          if (col === regionId) {
+            regions.push([i, j]);
+          }
+        })
+      );
+      //find all of its safe spaces
+      const safeSpaces = [];
+      regions.forEach(region => {
+        if (region[0] !== 0) {
+          //if not in first row
+          //check the top
+          if (initGrid[region[0] - 1][region[1]] === undefined) {
+            safeSpaces.push([region[0] - 1, region[1]]);
+          }
+        }
+        if (region[0] !== stars.length - 1) {
+          //if not in last row
+          //check the botom
+          if (initGrid[region[0] + 1][region[1]] === undefined) {
+            safeSpaces.push([region[0] + 1, region[1]]);
+          }
+        }
+        if (region[1] !== 0) {
+          //if not first column
+          //check the left
+          if (initGrid[region[0]][region[1] - 1] === undefined) {
+            safeSpaces.push([region[0], region[1] - 1]);
+          }
+        }
+        if (region[1] !== stars.length - 1) {
+          //if not last column
+          //check the right
+          if (initGrid[region[0]][region[1] + 1] === undefined) {
+            safeSpaces.push([region[0], region[1] + 1]);
+          }
+        }
+      });
+      if (safeSpaces.length) {
+        const space = safeSpaces[Math.floor(Math.random() * safeSpaces.length)];
+        initGrid[space[0]][space[1]] = regionId;
+      }
+    }
+
+    function isFilled(initGrid) {
+      let count = 0;
+      initGrid.forEach(row => {
+        row.forEach(col => {
+          if (col === undefined) {
+            count++;
+          }
+        });
+      });
+
+      return !count;
+    }
+    return initGrid;
+  };
+  const area = createAreas(grid);
+
+  console.log(area);
+  const distro = [];
+  area.forEach(row => row.forEach(col => (distro[col] = distro[col] + 1 || 1)));
+  console.log(distro);
+  return area;
+};
+
+export default getGrid;
